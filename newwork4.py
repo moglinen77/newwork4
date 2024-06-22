437fff488494666895251a48934c09cb506accee
moglinen77 <moglinen77@gmail.com>
2024-06-22 20:49:01 +0300

    Add files via upload

---
+++ newwork4.py
@@ -0,0 +1,9 @@
+my_string = 'Сколько стоят раки: '
+print(len('Сколько стоят раки: '))
+print(my_string)
+print(input(my_string))
+print(my_string .upper())
+print('НАША МАША ГРОМКО ПЛАЧЕТ'.lower())
+print(my_string .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
